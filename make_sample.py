22799e011b87cf92a1d80bf96688aa6795998ae0
aabelnegash <negashabel63@gmail.com>
2026-05-13 11:24:45 -0400

    Creating a reproducible random sample

--- make_sample.py
+++ make_sample.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 hospital_data = pd.read_csv("data/input/hospital_clean.csv")
 
-sample_data = hospital_data.head(200)
+sample_data = hospital_data.sample(n=200, random_state=1)
 
 sample_data.to_csv("data/sample/hospital_clean_sample.csv", index=False)
